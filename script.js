9327a7817e2bcecca914b2c0859f62f6afce13af
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-02-21 10:08:52 +0200

    Create initial Book constructor

---
+++ script.js
@@ -0,0 +1,10 @@
+const myLibrary = [];
+
+function Book() {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.isRead = function () {
+        // will add later
+    }
+}
