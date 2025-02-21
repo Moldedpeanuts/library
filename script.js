615ecef6c04b5ef96d2f689f18d02c2468b57b84
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-02-21 13:17:38 +0200

    Fix: div closing tags

--- script.js
+++ script.js
@@ -24,22 +24,26 @@ newBookBtn.addEventListener('click', () => {
         <form action method="POST">
             <label for="title">Title</label>
             <input type="text" name="title" id="title" required>
+
             <label for="author">Author</label>
             <input type="text" name="author" id="author" required>
+
             <label for="date">Publication Date</label>
             <input type="date" name="date" id="date"  required>
+            
             <label for="pages">Pages</label>
             <input type="number" name="pages" id="pages" required>
+
             <fieldset>
                 <legend>Have you read the book?</legend>
                 <div>
                     <input type="radio" id="book_is_read" name="read_book" value="true" checked>
                     <label for="book_is_read">Yes</label>
-                </div
+                </div>
                 <div>
                     <input type="radio" id="book_not_read" name="read_book" value="false">
                     <label for="book_not_read">Not yet</label>
-                </div
+                </div>
             </fieldset>
             <button type="submit" class="submit-button">Add New Book</button>
         </form>
