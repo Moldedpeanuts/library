368cf749f570c7bb7fccf7b645ea5dc0db966355
Sofia Makraki <160715585+Moldedpeanuts@users.noreply.github.com>
2025-02-21 12:15:14 +0200

    Add starting form

--- script.js
+++ script.js
@@ -6,5 +6,31 @@ function Book() {
     this.pages = pages;
     this.isRead = function () {
         // will add later
-    }
+    };
 }
+
+function addBookToLibrary(author, title, pages, isRead) {
+    const book = new Book(author, title, pages, isRead);
+}
+
+
+// Button add book form
+const contentDiv = document.querySelector('.content');
+const newBookBtn = document.querySelector('.add');
+
+newBookBtn.addEventListener('click', () => {
+    contentDiv.innerHTML = `
+    <div class="form-container">
+        <form>
+            <label for="title">Title</label>
+            <input type="text" name="title" id="title" required>
+            <label for="author">Author</label>
+            <input type="text" name="author" id="author" required>
+            <label for="date">Publication Date</label>
+            <input type="date" name="date" id="date"  required>
+            <label for="pages">Pages</label>
+            <input type="number" name="pages" id="pages" required>
+        </form>
+    </div>
+    `
+});
